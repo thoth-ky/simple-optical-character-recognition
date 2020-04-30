35f823a3b18c796d3d7725139b5e50ddd1d15915
Thoth Ky <28805113+thoth-ky@users.noreply.github.com>
2020-04-30 18:07:10 +0300

    add favicon endpoint

--- views/home.py
+++ views/home.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from os.path import join
+from flask import Blueprint, render_template, send_from_directory, current_app
 
 home_blueprint = Blueprint('home_page', __name__)
 
 @home_blueprint.route('/')
 def home_page():
     return render_template('index.html')
+
+
+@home_blueprint.route('/favicon.ico') 
+def favicon(): 
+    return send_from_directory(
+      join(current_app.root_path, 'static'),
+      'favicon.ico',
+      mimetype='image/vnd.microsoft.icon'
+    )
